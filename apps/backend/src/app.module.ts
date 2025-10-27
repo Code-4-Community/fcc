2d3469fc4e72b7349d4034572c4f02aa9116c483
Ben Petrillo <benpetrillo.bp@gmail.com>
2025-10-27 16:51:36 -0400

    feat: donations controller impl

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { DonationsModule } from './donations/donations.module';
 import AppDataSource from './data-source';
 
 @Module({
@@ -12,6 +13,7 @@ import AppDataSource from './data-source';
     TypeOrmModule.forRoot(AppDataSource.options),
     UsersModule,
     AuthModule,
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
