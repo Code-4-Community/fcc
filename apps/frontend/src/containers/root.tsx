289d8ee56b2935435bba781b1ec35025356afc8b
Gauri Rajesh <rajesh.ga@northeastern.edu>
2025-11-28 19:52:13 -0500

    gradient growth container handles

--- apps/frontend/src/containers/root.tsx
+++ apps/frontend/src/containers/root.tsx
@@ -4,7 +4,7 @@ import Plant from '@components/GrowingGoal/Plant';
 const Root: React.FC = () => {
   return (
     <div style={{ display: 'flex', gap: '2%' }}>
-      <div style={{ width: '15rem' }}>
+      <div style={{ width: '400rem' }}>
         <GrowingGoal
           message={'Grow your community with FCC!'}
           total={2000}
@@ -15,20 +15,31 @@ const Root: React.FC = () => {
           }}
         />
       </div>
-      <div style={{ width: '20rem' }}>
+      <div style={{ width: '400rem' }}>
         <GrowingGoal
           message={'Grow your community with FCC!'}
           total={4000}
           goal={10000}
           sampleDonation={{
-            name: 'long username keeps going on and on and on and on and on and on jkfdjfkjdkfdjfjd',
+            name: 'John Smith',
             amount: 567.987,
           }}
         />
       </div>
-      <div style={{ width: '25rem' }}>
+      <div style={{ width: '400rem' }}>
         <GrowingGoal
-          message={'Custom Message!'}
+          message={'Grow your community with FCC!'}
+          total={6000}
+          goal={10000}
+          sampleDonation={{
+            name: 'John Smith',
+            amount: 567.987,
+          }}
+        />
+      </div>
+      <div style={{ width: '400rem' }}>
+        <GrowingGoal
+          message={'Grow your community with FCC!'}
           total={8000}
           goal={10000}
           sampleDonation={{
@@ -37,14 +48,16 @@ const Root: React.FC = () => {
           }}
         />
       </div>
-      <div style={{ width: '20rem' }}>
-        <Plant />
-      </div>
-      <div style={{ width: '30rem' }}>
-        <Plant />
-      </div>
-      <div style={{ width: '40rem' }}>
-        <Plant />
+      <div style={{ width: '400rem' }}>
+        <GrowingGoal
+          message={'Grow your community with FCC!'}
+          total={10000}
+          goal={10000}
+          sampleDonation={{
+            name: 'John Smith',
+            amount: 567.987,
+          }}
+        />
       </div>
     </div>
   );
