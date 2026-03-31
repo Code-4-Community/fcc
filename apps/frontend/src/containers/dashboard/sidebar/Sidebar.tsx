aac6dd18b80c6c9ebccf329dde2e31cb2dd08e69
pujitakalinadhabhotla <161961520+pujitakalinadhabhotla@users.noreply.github.com>
2026-03-31 14:33:07 -0400

    Small change

--- apps/frontend/src/containers/dashboard/sidebar/Sidebar.tsx
+++ apps/frontend/src/containers/dashboard/sidebar/Sidebar.tsx
@@ -65,7 +65,7 @@ export default function Sidebar({
         className,
       )}
     >
-      {/* 🔹 Top Logo Section */}
+      {/*Top Logo Section */}
       <div className="flex h-[103px] w-full items-center gap-[11px] px-[22px] py-[9px]">
         <img src={FCCLogo} alt="FCC Logo" className="w-[56px] shrink-0" />
         <img
@@ -75,7 +75,7 @@ export default function Sidebar({
         />
       </div>
 
-      {/* 🔹 Navigation */}
+      {/*Navigation */}
       <nav className="flex w-full flex-col gap-0.05 px-4 pt-8">
         {navItems.map((item) => {
           const isActive = item.label === activeItem;
