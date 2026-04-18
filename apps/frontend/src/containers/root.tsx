9b18d58198711c34786aa0433797487f29a80be1
Thanin Kongkiatsophon <108406347+thaninbew@users.noreply.github.com>
2026-04-18 02:24:38 -0400

    feat: add back TestimonialCarousel with default banner (lost somehow)

--- apps/frontend/src/containers/root.tsx
+++ apps/frontend/src/containers/root.tsx
@@ -8,9 +8,6 @@ import {
 import { TestimonialCarousel } from '@components/testimonials/TestimonialCarousel';
 import { DonationForm } from './donations/DonationForm';
 import { useActiveGoal } from '../hooks/useActiveGoal';
-import CarouselImage1 from '@components/testimonials/TestimonialImages/Carousel_image1.png';
-import CarouselImage2 from '@components/testimonials/TestimonialImages/Carousel_image2.png';
-import CarouselImage3 from '@components/testimonials/TestimonialImages/Carousel_image3.png';
 
 const SAMPLE_DONATION: SampleDonation = {
   name: 'C4C',
@@ -18,24 +15,6 @@ const SAMPLE_DONATION: SampleDonation = {
   profile:
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAYAAAAfSC3RAAAALElEQVR42mNgGAWjgLGB4T8DGjBgFiMDw38GphCjEopFY1GNRg0Y1GgAAAD9YB5WfVii1AAAAABJRU5ErkJggg==',
 };
-
-const TESTIMONIAL_SLIDES = [
-  {
-    id: 1,
-    image: CarouselImage1,
-    alt: 'Testimonial image 1',
-  },
-  {
-    id: 2,
-    image: CarouselImage2,
-    alt: 'Testimonial image 2',
-  },
-  {
-    id: 3,
-    image: CarouselImage3,
-    alt: 'Testimonial image 3',
-  },
-];
 
 const Root: React.FC = () => {
   const { data } = useActiveGoal();
@@ -56,7 +35,7 @@ const Root: React.FC = () => {
     <div className="root-page">
       <div className="root-container">
         <section className="root-testimonial">
-          <TestimonialCarousel slides={TESTIMONIAL_SLIDES} />
+          <TestimonialCarousel />
         </section>
 
         <section className="root-content-grid">
