09f495447d7768d49f4129a2849645f2cdb79ffe
Thanin Kongkiatsophon <108406347+thaninbew@users.noreply.github.com>
2026-06-11 14:13:20 +0700

    refactor: rename goal columns to prevent confusion; + feat: only admins see admin control tab

---
+++ apps/backend/src/migrations/1781161660-rename-goal-columns.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class RenameGoalColumns1781161660 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "goals" RENAME COLUMN "targetAmount" TO "admin_target_amount"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "goals" RENAME COLUMN "admin_target_amount" TO "targetAmount"`,
+    );
+  }
+}
