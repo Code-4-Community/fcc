d265aca155ed8f86f595d5799a296aef5026b25e
Aaron Ashby <101434393+aaronashby@users.noreply.github.com>
2026-06-11 18:05:08 -0400

    fixed migration name

--- apps/backend/src/migrations/1781161660-rename-goal-columns.ts
+++ apps/backend/src/migrations/1781161660-rename-goal-columns.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class RenameGoalColumns1781161660 implements MigrationInterface {
+export class RenameGoalColumns1781161660000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "goals" RENAME COLUMN "targetAmount" TO "admin_target_amount"`,
